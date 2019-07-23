bb1de23dbe87947f9893bf756548b6c5126a3b5b
harrytwright <haroldtomwright@gmail.com>
2019-07-23 09:31:38 +0100

    [src] Fix for #5
    Taking advantage of JS's being fluid the code has also been cut down from 29 lines to now 11

--- src/emitter.js
+++ src/emitter.js
@@ -1,27 +1,9 @@
-import { DEBUG, ERROR, FATAL, INFO, TRACE, WARN } from 'bunyan';
+import { nameFromLevel, resolveLevel } from 'bunyan';
 
 function emitter(stream, level) {
   return function() {
-    switch (level) {
-      case DEBUG:
-        stream.debug(...arguments);
-        break;
-      case WARN:
-        stream.warn(...arguments);
-        break;
-      case INFO:
-        stream.info(...arguments);
-        break;
-      case ERROR:
-        stream.error(...arguments);
-        break;
-      case TRACE:
-        stream.info(...arguments);
-        break;
-      case FATAL:
-        stream.error(...arguments);
-        break;
-    }
+    const _level = nameFromLevel[resolveLevel(level)];
+    stream[_level](...arguments);
   };
 }
 
