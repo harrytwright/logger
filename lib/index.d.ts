43462ee0bb893b4117a91e43628c50e03e28f74b
harrytwright <haroldtomwright@gmail.com>
2022-01-20 12:47:03 +0000

    testing(): clean up
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -61,6 +61,7 @@ declare function warn(namespace: string, ...message: any[]): void
  * Log any message with the error level to the stream
  * */
 declare function error(namespace: string, context: object, ...message: any[]): void
+declare function error(namespace: string, error: Error, ...message: any[]): void
 declare function error(namespace: string, ...message: any[]): void
 
 /**
@@ -70,6 +71,22 @@ declare function silent(namespace: string, context: object, ...message: any[]): 
 declare function silent(namespace: string, ...message: any[]): void
 
 /**
+ * Set any configuration value
+ * */
+declare function set(key: 'app', value: string)
+declare function set(key: 'level', value: LogLevels)
+declare function set(key: 'stream', value: WriteStream)
+declare function set(key: 'maxRecordSize', value: number)
+
+/**
+ * Get any configuration value
+ * */
+declare function get(key: 'app'): string
+declare function get(key: 'level'): LogLevels
+declare function get(key: 'stream'): WriteStream
+declare function get(key: 'maxRecordSize'): number
+
+/**
  * Pause the logger, saving all messages inside the buffer for when it is restarted
  * */
 declare function pause(): void
@@ -77,7 +94,7 @@ declare function pause(): void
 /**
  * Restart the logger, pushing out the buffered messages first
  * */
-declare function restart(): void
+declare function resume(): void
 
 /**
  * Set/Get the log level
@@ -118,12 +135,77 @@ declare class Log extends EventEmitter {
     // The record holder for the logger
     record: any[]
 
-    constructor(app: string, level: LogLevels)
+    constructor(app?: string, level?: LogLevels)
 
     // Pause the logger
     pause(): void
 
     // Start the logger back up
     resume(): void
+
+    /**
+     * Log any message with the 'silly' level to the stream
+     * */
+    silly(namespace: string, context: object, ...message: any[]): void
+    silly(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'verbose' level to the stream
+     * */
+    verbose(namespace: string, context: object, ...message: any[]): void
+    verbose(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'info' level to the stream
+     * */
+    info(namespace: string, context: object, ...message: any[]): void
+    info(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'timing' level to the stream
+     * */
+    timing(namespace: string, context: object, ...message: any[]): void
+    timing(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'http' level to the stream
+     * */
+    http(namespace: string, context: object, ...message: any[]): void
+    http(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'notice' level to the stream
+     * */
+    notice(namespace: string, context: object, ...message: any[]): void
+    notice(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'warn' level to the stream
+     * */
+    warn(namespace: string, context: object, ...message: any[]): void
+    warn(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'error' level to the stream
+     * */
+    error(namespace: string, context: object, ...message: any[]): void
+    error(namespace: string, error: Error, ...message: any[]): void
+    error(namespace: string, ...message: any[]): void
+
+    /**
+     * Log any message with the 'silent' level to the stream
+     * */
+    silent(namespace: string, context: object, ...message: any[]): void
+    silent(namespace: string, ...message: any[]): void
+
+    set(key: 'app', value: string)
+    set(key: 'level', value: LogLevels)
+    set(key: 'stream', value: WriteStream)
+    set(key: 'maxRecordSize', value: number)
+
+    get(key: 'app'): string
+    get(key: 'level'): LogLevels
+    get(key: 'stream'): WriteStream
+    get(key: 'maxRecordSize'): number
 }
 
--- test/logging.test.js
+++ test/logging.test.js
@@ -25,10 +25,13 @@ function customStream () {
 }
 
 describe('logging', function () {
+  let prev = { ...process.env }
+
   before(function () {
     // No need to check stdin either
     // just wipe this
     logger.record = []
+    logger.stream = process.stderr
   })
 
   describe('namespace is set', function () {
@@ -50,15 +53,76 @@ describe('logging', function () {
       expect(record.context).to.be.deep.equal({ hello: 'world' })
     })
   })
+
+  describe('pausing', function () {
+    before(() => {
+      process.env.__testing_overide = false
+      logger.stream = customStream()
+    })
+
+    after(() => {
+      process.env = prev
+      logger.stream = process.stderr
+    })
+
+    it('should populate the buffer', function () {
+      logger.pause()
+
+      logger.info('context', { hello: 'world' }, 'something informative')
+      expect(logger.$__buffer).to.not.be.empty
+
+      logger.resume()
+      expect(logger.stream.records).to.not.be.empty
+    })
+
+    it('should return empty on resuming', function () {
+      expect(() => logger.resume()).to.not.throw()
+    })
+  })
+
+  describe('empty logging', function () {
+    before(() => {
+      process.env.__testing_overide = false
+      logger.stream = customStream()
+    })
+
+    after(() => {
+      process.env = prev
+      logger.stream = process.stderr
+    })
+
+    it('should throw', function () {
+      expect(() => logger.info()).to.throw()
+    })
+
+    it('should handle empty namespace', function () {
+      expect(() => logger.info(null)).to.not.throw()
+    })
+
+    // This is more for coverage checks
+    it('should handle empty app with trace', function () {
+      let app = logger.app
+      logger.app = null
+
+      expect(() => logger.info(null, { trace: 'qwertyuiop' })).to.not.throw()
+      logger.app = app
+    })
+  })
 })
 
 describe('custom logger', function () {
-  let log, stream
+  let log, stream, prev = { ...process.env }
 
   before(function () {
+    process.env.__testing_overide = false
+
     stream = customStream()
     log = new logger.Log('custom', 'notice')
     log.stream = stream
+  })
+
+  after(function () {
+    process.env = prev
   })
 
   it('should log the message', function () {
@@ -69,5 +133,106 @@ describe('custom logger', function () {
   it('should not log the message', function () {
     log.silly('namespace', 'message')
     expect(stream.records).to.have.length(1)
+  })
+})
+
+describe('configuration', function () {
+  it('should update max record size', function () {
+    const Log = logger.Log
+
+    const log = new Log('maxRecordSize', 'silly')
+    log.set('maxRecordSize', 2)
+
+    log.silly('namespace', 'message')
+    log.silly('namespace', 'message')
+    log.silly('namespace', 'message')
+    log.silly('namespace', 'message')
+
+    expect(log.record).to.have.length(2)
+  })
+
+  it('should get/set app name', function () {
+    let appName = 'should get/set app name'
+
+    const Log = logger.Log
+    const log = new Log()
+
+    log.set('app', appName)
+    expect(log.get('app')).to.be.eq(appName)
+
+    log.app = appName
+    expect(log.app).to.be.eq(appName)
+  })
+
+  it('should get/set app level', function () {
+    let appLevel = 'silent'
+
+    const Log = logger.Log
+    const log = new Log()
+
+    log.set('level', appLevel)
+    expect(log.get('level')).to.be.eq(appLevel)
+
+    log.level = appLevel
+    expect(log.level).to.be.eq(appLevel)
+  })
+
+  it('should get/set app stream', function () {
+    let stream = process.stdout
+
+    const Log = logger.Log
+    const log = new Log()
+
+    log.set('stream', stream)
+    expect(log.get('stream')).to.be.deep.eq(stream)
+
+    log.stream = stream
+    expect(log.stream).to.be.deep.eq(stream)
+  })
+})
+
+describe('error-logging', function () {
+  before(function () {
+    // No need to check stdin either
+    // just wipe this
+    logger.record = []
+  })
+
+  it('should log a valid error', function () {
+    let error = new Error('error')
+    logger.error('namespace', error)
+
+    const record = logger.record.pop()
+    expect(error.stack).to.be.eq(record.context.stack)
+  })
+})
+
+/**
+ * This is a very subjective use-case but allows for in dev view traces to be clumped with a set format
+ *
+ * May look at custom formats in the future?? Like how morgan works but allowing for custom keys maybe like
+ * how you can use string interpolation, or maybe even that ??
+ * */
+describe('tracing', function () {
+  let log, stream, prev = { ...process.env }
+
+  before(function () {
+    stream = customStream()
+    log = new logger.Log('custom', 'silly')
+    log.stream = stream
+  })
+
+  after(function () {
+    process.env = prev
+  })
+
+  it('should log a valid trace', function () {
+    process.env.__testing_overide = false
+    log.info('namespace', { trace: '12345678910' }, 'Hello world')
+
+    const record = log.record.pop()
+    expect(record.context.trace).to.be.eq('12345678910')
+
+    expect(stream.records.pop()).to.contains('12345678910')
   })
 })
