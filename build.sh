e2e3a312f7bb36ddf2248070718ce818a2391071
harrytwright <haroldtomwright@gmail.com>
2022-01-25 01:34:33 +0000

    src: Move to full minified code
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 
 mv ./package.json ./fake.package.json
 
+mkdir dist
+
 # This is a contrivance, using the transformer, as there is no alternative other than process.env
 # being manipulated another way
 browserify --outfile logger.js -t [ envify --NODE_ENV production  ] --standalone logger --entry ./lib/index.js
 
 # Minify the code
 google-closure-compiler --js logger.js --js_output_file logger.min.js --strict_mode_input=false
+
+# Minify the logger code
+cp ./lib/index.d.ts ./dist/index.d.ts
+google-closure-compiler --js ./lib/index.js --js_output_file ./dist/index.js --strict_mode_input=false
 
 mv ./fake.package.json ./package.json
