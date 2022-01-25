d8334289703e745d458ba1a807d163c69ec541ae
harrytwright <haroldtomwright@gmail.com>
2022-01-25 01:24:10 +0000

    src: Add extra testing for browser
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 mv ./package.json ./fake.package.json
 
-browserify --outfile logger.js --standalone logger --entry ./lib/index.js
+# This is a contrivance, using the transformer, as there is no alternative other than process.env
+# being manipulated another way
+browserify --outfile logger.js -t [ envify --NODE_ENV production  ] --standalone logger --entry ./lib/index.js
 
 # Minify the code
 google-closure-compiler --js logger.js --js_output_file logger.min.js --strict_mode_input=false
--- test/logging.browser.test.js
+++ test/logging.browser.test.js
@@ -5,3 +5,36 @@ describe('name', function () {
     expect(logger.app).to.be.eq(window.origin)
   })
 })
+
+describe('redaction', function () {
+  const expect = chai.expect
+
+  const prev = { ...process.env }
+
+  before(() => {
+    process.env.__testing_overide = true
+  })
+
+  after(() => {
+    process.env = prev
+  })
+
+  it('should throw on invalid redaction', function () {
+    expect(() => logger.redaction(5)).to.throw()
+  })
+
+  it('should add a custom redaction', function () {
+    logger.redaction((value) => {
+      if (typeof value === 'number') return (value >>> 0).toString(2)
+      return value
+    })
+
+    // We can't test this as there is no way to change the production env here
+    expect(logger.redactions).to.have.lengthOf(1)
+
+    logger.info('demo', 'binary output for 5 is %s', 10)
+
+    const record = logger.record.pop()
+    expect(record.message).to.be.eq('binary output for 5 is 1010')
+  })
+})
