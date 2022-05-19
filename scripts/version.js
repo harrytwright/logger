49c6b81de9b8b87263658911fc69bd6c5a5e2aeb
harrytwright <haroldtomwright@gmail.com>
2022-05-19 13:05:53 +0100

    scripts: Fix requires
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- scripts/version.js
+++ scripts/version.js
@@ -1,4 +1,4 @@
-const npmVersion = require('node:child_process').spawnSync('npm', ['--version']).stdout.toString().trim()
+const npmVersion = require('child_process').spawnSync('npm', ['--version']).stdout.toString().trim()
 
 if (require('semver').satisfies(npmVersion, '>=7.x')) return console.log('TRUE')
 console.log('FALSE')
