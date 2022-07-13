2d0c79be032fb2a2bf2089c3c61c3129cdb084e0
harrytwright <haroldtomwright@gmail.com>
2022-07-13 12:23:54 +0100

    sh: Change build script
    Move to taking out NODE_ENV for browser as this should still be set by the user, as JSON logs in the browser are a pain in the ass to read
    
    And add some colours
    
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
+
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
 mv ./package.json ./fake.package.json
 
 mkdir dist
 
 # This is a contrivance, using the transformer, as there is no alternative other than process.env
 # being manipulated another way
-browserify --outfile logger.js -t [ envify --NODE_ENV production  ] --standalone logger --entry ./lib/index.js
+# -t [ envify --NODE_ENV null  ]
+echo "$ ${GREEN}browserify${RESET} --outfile logger.js --standalone logger --entry ./lib/index.js"
+browserify --outfile logger.js --standalone logger --entry ./lib/index.js || exit 1
 
 # Minify the code
+echo "$ ${GREEN}google-closure-compiler${RESET} --js logger.js --js_output_file logger.min.js --strict_mode_input=false"
 google-closure-compiler --js logger.js --js_output_file logger.min.js --strict_mode_input=false
 
 # Minify the logger code
+echo "$ ${GREEN}google-closure-compiler${RESET} --js ./lib/index.js --js_output_file ./dist/index.js --strict_mode_input=false"
 cp ./lib/index.d.ts ./dist/index.d.ts
 google-closure-compiler --js ./lib/index.js --js_output_file ./dist/index.js --strict_mode_input=false
 
